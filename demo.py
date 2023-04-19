5ce5fa6b378eed1cba0b1e0c7b6671500be65b3e
Vishal boraste <vishalboraste321@gmail.com>
2023-04-19 16:07:58 +0530

    logging file completed

---
+++ demo.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from visa.logger import logging
+
+app = Flask(__name__)
+
+@app.route('/', methods =['GET','POST'])
+
+def index():
+    logging.info("We are testing our logging file ")
+    return "Hello World Vishal"
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ visa/logger/__init__.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import logging
+import os , sys
+
+LOG_DIR = "logs"
+
+CURRENT_TIME_STAMP = f"log{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+
+log_file_name =f"logs{CURRENT_TIME_STAMP}.log"
+
+os.makedirs(LOG_DIR,exist_ok=True)
+
+log_file_path = os.path.join(LOG_DIR,log_file_name)
+
+logging.basicConfig(filename = log_file_path,
+                    filemode="w",
+                    format = '[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO
+                    )
